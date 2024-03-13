4ea430009c5d05f1d3166013d241d81c7d6608e9
Jonathan Nobels <jonathan@tailscale.com>
2024-03-13 16:05:31 -0400

    android: remove IPNActivity and move any explicit functionality to MainActivity
    Removes the original IPNActivity class and moves the Intent related functionality over to MainActivity.   That all needs to be revisited when we reimplmenet TailDrop with localAPI.

--- android/src/main/java/com/tailscale/ipn/MainActivity.kt
+++ android/src/main/java/com/tailscale/ipn/MainActivity.kt
@@ -3,11 +3,12 @@
 
 package com.tailscale.ipn
 
-import android.content.Intent
-import android.net.Uri
 import android.content.Context
+import android.content.Intent
 import android.content.RestrictionsManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
@@ -38,6 +39,10 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        public const val WRITE_STORAGE_RESULT = 1000
+    }
+
     private val manager = IpnManager(lifecycleScope)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,23 +53,23 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "main") {
                     val mainViewNav = MainViewNavigation(
-                        onNavigateToSettings = { navController.navigate("settings") },
-                        onNavigateToPeerDetails = {
-                            navController.navigate("peerDetails/${it.StableID}")
-                        },
-                        onNavigateToExitNodes = { navController.navigate("exitNodes") }
+                            onNavigateToSettings = { navController.navigate("settings") },
+                            onNavigateToPeerDetails = {
+                                navController.navigate("peerDetails/${it.StableID}")
+                            },
+                            onNavigateToExitNodes = { navController.navigate("exitNodes") }
                     )
 
                     val settingsNav = SettingsNav(
-                        onNavigateToBugReport = { navController.navigate("bugReport") },
-                        onNavigateToAbout = { navController.navigate("about") },
-                        onNavigateToMDMSettings = { navController.navigate("mdmSettings") }
+                            onNavigateToBugReport = { navController.navigate("bugReport") },
+                            onNavigateToAbout = { navController.navigate("about") },
+                            onNavigateToMDMSettings = { navController.navigate("mdmSettings") }
                     )
 
                     composable("main") {
                         MainView(
-                            viewModel = MainViewModel(manager.model, manager),
-                            navigation = mainViewNav
+                                viewModel = MainViewModel(manager.model, manager),
+                                navigation = mainViewNav
                         )
                     }
                     composable("settings") {
@@ -74,14 +79,14 @@ class MainActivity : ComponentActivity() {
                         ExitNodePicker(ExitNodePickerViewModel(manager.model))
                     }
                     composable(
-                        "peerDetails/{nodeId}",
-                        arguments = listOf(navArgument("nodeId") { type = NavType.StringType })
+                            "peerDetails/{nodeId}",
+                            arguments = listOf(navArgument("nodeId") { type = NavType.StringType })
                     ) {
                         PeerDetails(
-                            PeerDetailsViewModel(
-                                manager.model, nodeId = it.arguments?.getString("nodeId")
-                                    ?: ""
-                            )
+                                PeerDetailsViewModel(
+                                        manager.model, nodeId = it.arguments?.getString("nodeId")
+                                        ?: ""
+                                )
                         )
                     }
                     composable("bugReport") {
@@ -96,6 +101,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        handleIntent()
     }
 
     init {
@@ -124,8 +130,73 @@ class MainActivity : ComponentActivity() {
     override fun onResume() {
         super.onResume()
         val restrictionsManager =
-            this.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
+                this.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
         manager.mdmSettings = MDMSettings(restrictionsManager)
+    }
+
+
+    override fun onNewIntent(i: Intent?) {
+        super.onNewIntent(i)
+        intent = i
+        handleIntent()
+    }
+
+
+    // (jonathan) TODO: Copied from the original IPNActivity, presumably to support Taildrop.
+    // This should be refactored and cleaned up to remove the deprecations.
+
+    private fun handleIntent() {
+        val it = intent
+        val act = it.action
+        val texts: Array<String?>
+        val uris: Array<Uri?>
+        if (Intent.ACTION_SEND == act) {
+            uris = arrayOf(it.getParcelableExtra(Intent.EXTRA_STREAM))
+            texts = arrayOf(it.getStringExtra(Intent.EXTRA_TEXT))
+        } else if (Intent.ACTION_SEND_MULTIPLE == act) {
+            val extraUris: List<Uri?>? = it.getParcelableArrayListExtra(Intent.EXTRA_STREAM)
+            uris = extraUris!!.toTypedArray<Uri?>()
+            texts = arrayOfNulls(uris.size)
+        } else {
+            return
+        }
+        val mime = it.type
+        val nitems = uris.size
+        val items = arrayOfNulls<String>(nitems)
+        val mimes = arrayOfNulls<String>(nitems)
+        val types = IntArray(nitems)
+        val names = arrayOfNulls<String>(nitems)
+        val sizes = LongArray(nitems)
+        var nfiles = 0
+        for (i in uris.indices) {
+            val text = texts[i]
+            val uri = uris[i]
+            if (text != null) {
+                types[nfiles] = 1 // FileTypeText
+                names[nfiles] = "file.txt"
+                mimes[nfiles] = mime
+                items[nfiles] = text
+                // Determined by len(text) in Go to eliminate UTF-8 encoding differences.
+                sizes[nfiles] = 0
+                nfiles++
+            } else if (uri != null) {
+                val c = contentResolver.query(uri, null, null, null, null)
+                        ?: // Ignore files we have no permission to access.
+                        continue
+                val nameCol = c.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                val sizeCol = c.getColumnIndex(OpenableColumns.SIZE)
+                c.moveToFirst()
+                val name = c.getString(nameCol)
+                val size = c.getLong(sizeCol)
+                types[nfiles] = 2 // FileTypeURI
+                mimes[nfiles] = mime
+                items[nfiles] = uri.toString()
+                names[nfiles] = name
+                sizes[nfiles] = size
+                nfiles++
+            }
+        }
+        App.onShareIntent(nfiles, types, mimes, items, names, sizes)
     }
 }
 
